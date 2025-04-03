f2a79a452a586b38f8b8f3415bb7ef9e59597181
AliCMA <32778@ma-web.nl>
2025-04-03 12:17:06 +0200

    Logo, titel, en eerste input veld van de regrister page

--- src/app/Regrister/page.tsx
+++ src/app/Regrister/page.tsx
@@ -1,5 +1,17 @@
 import styles from "./regrister.module.scss";
 
 export default function Register() {
-    
+    return(
+        <div className={styles.container}>
+            <div className={styles.registerBox}>
+                <img src="/logo.png" alt="Logo" className={styles.logo} />
+                <h2 className={styles.title}>Register</h2>
+                <input
+                    type="text"
+                    placeholder="Company name"
+                    className={styles.inputField}
+                />
+        </div>
+        </div>
+    )
 }
