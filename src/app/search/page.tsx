e0082728685d624ec155bd0a0524e61d883d3890
AliCMA <32778@ma-web.nl>
2025-04-07 09:45:02 +0200

    React-functionele component

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -3,6 +3,12 @@
 import styles from "./search.module.scss";
 
 export default function Search() {
+  const handleEditUser = (userIndex: number) => {
+    console.log(`Edit user ${userIndex + 1}`);
+    //Hier komt de back-end voor de functie toeveogen om een gebruiker te bewerken
+  };
+
+
   return (
     <div className={styles.search}>
       <h1>Search</h1>
