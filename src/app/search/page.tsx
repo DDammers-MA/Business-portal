1c3351c03c4b2668cd91df1c7ecf57ac4088a5e8
AliCMA <32778@ma-web.nl>
2025-04-07 09:42:23 +0200

    Settup voor de search front-end

---
+++ src/app/search/page.tsx
@@ -0,0 +1,12 @@
+"use client"; 
+
+import styles from "./search.module.scss";
+
+export default function Search() {
+  return (
+    <div className={styles.search}>
+      <h1>Search</h1>
+      <p>Search page content goes here.</p>
+    </div>
+  );
+}
