9e52f40c1c4aef838b18a3277cf45b1d797274d0
AliCMA <32778@ma-web.nl>
2025-04-07 10:49:49 +0200

    file name veranderd

---
+++ src/app/users/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import styles from "./user.module.scss";
+
+export default function Search() {
+  const handleEditUser = (userIndex: number) => {
+    console.log(`Edit user ${userIndex + 1}`);
+    //Hier komt de back-end voor de functie toeveogen om een gebruiker te bewerken
+  };
+
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.searchBar}>
+          <i className="fa-solid fa-magnifying-glass"></i>
+          <input
+            type="text"
+            placeholder="Search users"
+            className={styles.inputField}
+          />
+        </div>
+        <button className={styles.addUserButton}>Add new user</button>
+      </div>
+      <div className={styles.userList}>
+        {Array.from({ length: 6 }).map((_, index) => (
+          <div key={index} className={styles.userRow}>
+         <i className={`fa-regular fa-circle-user ${styles.userIcon}`}></i>
+            <span className={styles.userName}>John Doe</span> {}
+            <i
+              className={`fa-regular fa-pen-to-square ${styles.editIcon}`}
+              onClick={() => handleEditUser(index)} 
+            ></i>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
