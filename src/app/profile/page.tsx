2da3f143eff6a106c65e4a71048979d904a6aef1
Daniel <33257@ma-web.nl>
2025-04-24 09:49:30 +0200

    buttin chenge

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -122,137 +122,150 @@ const ProfilePage = () => {
   };
 
   return (
-    <div className={styles.profileContainer}>
-      <h1 className={styles.title}>Mijn Profiel</h1>
+<div className={styles.profileContainer}>
+  <h1 className={styles.title}>Mijn Profiel</h1>
 
-      {error && <p className={styles.error}>{error}</p>}
+  {error && <p className={styles.error}>{error}</p>}
 
-      <div className={styles.profileInfo}>
-  <div className={styles.infoItem}>
-    <strong>Bedrijfsnaam:</strong>
-    {isEditing ? (
-      <input name="companyName" value={profile.companyName} onChange={handleInputChange} />
-    ) : (
-      <span>{profile.companyName}</span>
-    )}
-  </div>
-  <div className={styles.infoItem}>
-    <strong>Bedrijfsemail:</strong>
-    {isEditing ? (
-      <input name="companyEmail" value={profile.companyEmail} onChange={handleInputChange} />
-    ) : (
-      <span>{profile.companyEmail}</span>
-    )}
-  </div>
-  <div className={styles.infoItem}>
-    <strong>KvK-nummer:</strong>
-    {isEditing ? (
-      <input name="kvkNumber" value={profile.kvkNumber} onChange={handleInputChange} />
-    ) : (
-      <span>{profile.kvkNumber}</span>
-    )}
-  </div>
-  <div className={styles.infoItem}>
-    <strong>Telefoonnummer:</strong>
-    {isEditing ? (
-      <input name="phoneNumber" value={profile.phoneNumber} onChange={handleInputChange} />
-    ) : (
-      <span>{profile.phoneNumber}</span>
-    )}
-  </div>
-      </div>
-      
+  <div className={styles.profileInfo}>
+    <div className={styles.infoItem}>
+      <strong>Bedrijfsnaam:</strong>
       {isEditing ? (
-  <div className={styles.buttonGroup}>
-    <button className={styles.saveButton} onClick={handleSaveChanges}>
-      Opslaan
-    </button>
-    <button className={styles.cancelButton} onClick={() => setIsEditing(false)}>
-      Annuleren
-    </button>
-  </div>
-) : (
-  <button className={styles.toggleButton} onClick={() => setIsEditing(true)}>
-    Profiel bewerken
-  </button>
-)}
-
-
-      <button
-        className={styles.toggleButton}
-        onClick={() => setShowPasswordForm(!showPasswordForm)}
-      >
-        {showPasswordForm ? 'Annuleren' : 'Wachtwoord wijzigen'}
-      </button>
-
-      {showPasswordForm && (
-        <div className={styles.passwordSection}>
-          <h2 className={styles.subtitle}>Wachtwoord wijzigen</h2>
-          <form className={styles.passwordForm} onSubmit={handlePasswordChange}>
-            <div className={styles.formGroup}>
-              <label htmlFor="currentPassword">Huidig wachtwoord</label>
-              <div className={styles.passwordInput}>
-                <input
-                  type={showCurrentPassword ? 'text' : 'password'}
-                  id="currentPassword"
-                  value={currentPassword}
-                  onChange={(e) => setCurrentPassword(e.target.value)}
-                  required
-                />
-                <span
-                  className={styles.showText}
-                  onClick={() => setShowCurrentPassword(!showCurrentPassword)}
-                >
-                  {showCurrentPassword ? 'Hide' : 'Show'}
-                </span>
-              </div>
-            </div>
-
-            <div className={styles.formGroup}>
-              <label htmlFor="newPassword">Nieuw wachtwoord</label>
-              <div className={styles.passwordInput}>
-                <input
-                  type={showNewPassword ? 'text' : 'password'}
-                  id="newPassword"
-                  value={newPassword}
-                  onChange={(e) => setNewPassword(e.target.value)}
-                  required
-                />
-                <span
-                  className={styles.showText}
-                  onClick={() => setShowNewPassword(!showNewPassword)}
-                >
-                  {showNewPassword ? 'Hide' : 'Show'}
-                </span>
-              </div>
-            </div>
-
-            <div className={styles.formGroup}>
-              <label htmlFor="confirmPassword">Bevestig nieuw wachtwoord</label>
-              <div className={styles.passwordInput}>
-                <input
-                  type={showConfirmPassword ? 'text' : 'password'}
-                  id="confirmPassword"
-                  value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                  required
-                />
-                <span
-                  className={styles.showText}
-                  onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-                >
-                  {showConfirmPassword ? 'Hide' : 'Show'}
-                </span>
-              </div>
-            </div>
-
-            <button type="submit" className={styles.submitButton}>
-              Wachtwoord wijzigen
-            </button>
-          </form>
-        </div>
+        <input name="companyName" value={profile.companyName} onChange={handleInputChange} />
+      ) : (
+        <span>{profile.companyName}</span>
       )}
     </div>
+    <div className={styles.infoItem}>
+      <strong>Bedrijfsemail:</strong>
+      {isEditing ? (
+        <input name="companyEmail" value={profile.companyEmail} onChange={handleInputChange} />
+      ) : (
+        <span>{profile.companyEmail}</span>
+      )}
+    </div>
+    <div className={styles.infoItem}>
+      <strong>KvK-nummer:</strong>
+      {isEditing ? (
+        <input name="kvkNumber" value={profile.kvkNumber} onChange={handleInputChange} />
+      ) : (
+        <span>{profile.kvkNumber}</span>
+      )}
+    </div>
+    <div className={styles.infoItem}>
+      <strong>Telefoonnummer:</strong>
+      {isEditing ? (
+        <input name="phoneNumber" value={profile.phoneNumber} onChange={handleInputChange} />
+      ) : (
+        <span>{profile.phoneNumber}</span>
+      )}
+    </div>
+  </div>
+
+      <div className={styles.buttonGroup}>
+        
+        <div className={styles.buttonGroup__btns}>
+
+       
+    {isEditing ? (
+      <>
+        <button className={styles.saveButton} onClick={handleSaveChanges}>
+          Opslaan
+        </button>
+        <button className={styles.cancelButton} onClick={() => setIsEditing(false)}>
+          Annuleren
+        </button>
+      </>
+    ) : (
+      <button className={styles.toggleButton} onClick={() => setIsEditing(true)}>
+        Profiel bewerken
+      </button>
+          )}
+          
+        </div>
+        
+        <div>
+
+      
+
+    <button
+      className={styles.toggleButton}
+      onClick={() => setShowPasswordForm(!showPasswordForm)}
+    >
+      {showPasswordForm ? 'Annuleren' : 'Wachtwoord wijzigen'}
+          </button>
+          
+          </div>
+  </div>
+
+  {showPasswordForm && (
+    <div className={styles.passwordSection}>
+      <h2 className={styles.subtitle}>Wachtwoord wijzigen</h2>
+      <form className={styles.passwordForm} onSubmit={handlePasswordChange}>
+        <div className={styles.formGroup}>
+          <label htmlFor="currentPassword">Huidig wachtwoord</label>
+          <div className={styles.passwordInput}>
+            <input
+              type={showCurrentPassword ? 'text' : 'password'}
+              id="currentPassword"
+              value={currentPassword}
+              onChange={(e) => setCurrentPassword(e.target.value)}
+              required
+            />
+            <span
+              className={styles.showText}
+              onClick={() => setShowCurrentPassword(!showCurrentPassword)}
+            >
+              {showCurrentPassword ? 'Hide' : 'Show'}
+            </span>
+          </div>
+        </div>
+
+        <div className={styles.formGroup}>
+          <label htmlFor="newPassword">Nieuw wachtwoord</label>
+          <div className={styles.passwordInput}>
+            <input
+              type={showNewPassword ? 'text' : 'password'}
+              id="newPassword"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+              required
+            />
+            <span
+              className={styles.showText}
+              onClick={() => setShowNewPassword(!showNewPassword)}
+            >
+              {showNewPassword ? 'Hide' : 'Show'}
+            </span>
+          </div>
+        </div>
+
+        <div className={styles.formGroup}>
+          <label htmlFor="confirmPassword">Bevestig nieuw wachtwoord</label>
+          <div className={styles.passwordInput}>
+            <input
+              type={showConfirmPassword ? 'text' : 'password'}
+              id="confirmPassword"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
+            <span
+              className={styles.showText}
+              onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+            >
+              {showConfirmPassword ? 'Hide' : 'Show'}
+            </span>
+          </div>
+        </div>
+
+        <button type="submit" className={styles.submitButton}>
+          Wachtwoord wijzigen
+        </button>
+      </form>
+    </div>
+  )}
+</div>
   );
 };
 
